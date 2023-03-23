d8979af121a162bda1e10588c216cb98f0393935
subh123sultanpur <india2039ballia@gmail.com>
2023-03-23 15:28:31 +0530

    reactapp

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navigation from "./component/navigation";
+
+function App() { 
+    return (
+    <div className="container">
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/navigation.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function navigation() {
+  return <nav className="nav">Nav</nav>;
+}
